e3b102728a188cad9cf587c7c3ea5e427eb00758
Hamza Saleh <hamzasaleh102938@gmail.com>
2025-12-26 22:00:16 +0200

    updated

--- src/main/java/com/hamza/stadiumbooking/security/config/CacheConfig.java
+++ src/main/java/com/hamza/stadiumbooking/security/config/CacheConfig.java
@@ -1,5 +1,6 @@
 package com.hamza.stadiumbooking.security.config;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
@@ -9,6 +10,7 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 
 @Configuration
 @EnableCaching
+@ConditionalOnProperty(name = "spring.cache.type", havingValue = "redis")
 public class CacheConfig {
 
     @Bean
