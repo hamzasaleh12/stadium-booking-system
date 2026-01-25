325f7dba7674eced2a7949519fa842a7700ee414
Hamza Saleh <hamzasaleh102938@gmail.com>
2026-01-25 20:45:53 +0200

    feat: enable database seeding and configure swagger ui path

--- src/main/java/com/hamza/stadiumbooking/StadiumBookingSystemApplication.java
+++ src/main/java/com/hamza/stadiumbooking/StadiumBookingSystemApplication.java
@@ -40,4 +40,30 @@ public class StadiumBookingSystemApplication {
         return new BCryptPasswordEncoder();
     }
 
+    @Bean
+    CommandLineRunner runDatabaseSeeder(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder) {
+        return args -> {
+            seedUserIfNotExist(userRepository, passwordEncoder, "Super Admin", "admin@gmail.com", "Admin@1234", "01012345678", Role.ROLE_ADMIN);
+            seedUserIfNotExist(userRepository, passwordEncoder, "Manager User", "manager@gmail.com", "Manager@1234", "01022345678", Role.ROLE_MANAGER);
+            seedUserIfNotExist(userRepository, passwordEncoder, "Player User", "player@gmail.com", "Player@1234", "01032345678", Role.ROLE_PLAYER);
+        };
+    }
+
+    private void seedUserIfNotExist(UserRepository repo, BCryptPasswordEncoder encoder,
+                                    String name, String email, String password, String phone, Role role) {
+        if (repo.findByEmailAndIsDeletedFalse(email).isEmpty()) {
+            User user = User.builder()
+                    .name(name)
+                    .email(email)
+                    .password(encoder.encode(password))
+                    .phoneNumber(phone)
+                    .role(role)
+                    .dob(LocalDate.of(1995, 1, 1))
+                    .isDeleted(false)
+                    .build();
+
+            repo.save(user);
+            log.info("✅ {} Seeded: {} / {}", role, email, password);
+        }
+    }
 }
